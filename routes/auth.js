4aa80bab272f0c13797be91cbd4260e219b316fc
kojoyeboah53i <yeboah.determined.isaac@gmail.com>
2021-07-28 19:35:53 +0000

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+const User = require('../model/User');
 
 //post register
 router.post('/register', (req, res) => {
-    res.send('register');
+    // res.send('register');
+    const user = new User({
+        name : req.body.name,
+        email : req.body.email,
+        password : req.body.password
+    })
 });
 
 //post login
